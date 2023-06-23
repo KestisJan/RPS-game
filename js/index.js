eb911dc2580fd0a027bd41d1649fca160a211a52
Kestas <kestutisjankevicius9@gmail.com>
2023-06-23 13:27:15 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,48 @@
-textDOM = document.querySelector(`text`);
+const buttonsDOM = document.querySelectorAll(`.buttons`);
+const textDOM = document.querySelector(`.text`);
+
+const npcDOM = document.querySelector(`.npc`);
+const playerDOM = document.querySelector(`.player`)
+
+const npcIndex = [`rock`,`paper`,`scissors`]
+
+for(const buttonDOM of buttonsDOM) {
+    let random = ``;
+    let userInput = ``;
+    buttonDOM.addEventListener(`click`, () => {
+        userInput = buttonDOM.innerHTML;
+        random = npcIndex[Math.floor(Math.random() * 3 )];
+       
+        if (userInput === random) {
+            console.log(`Its a tie`);
+        }else if (userInput === `Rock`) {
+            if (random === `scissors`) {
+                textDOM.innerHTML = `User wins`;
+            } else {
+                textDOM.innerHTML = `PC wins`;
+            }
+        }else if (userInput === `Paper`) {
+            if (random === `rock`) {
+                textDOM.innerHTML =`User wins`;
+            } else {
+                textDOM.innerHTML = `PC wins`;
+            }
+        } else {
+          if (userInput === `Scissor`) {
+            if (random === `paper`) {
+                textDOM.innerHTML = `User wins`;
+            } else {
+                textDOM.innerHTML = `PC wins`;
+            }
+         } 
+        } 
+    })  
+}
+
+
+
+
+
 
 
 
